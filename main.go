448fd6a96a434dcce0c8fe17927cdb282acd76c7
David Araújo <github@davidjosearaujo.dev>
2025-11-02 00:17:42 +0000

    Fix version number in root command to 0.1.1

--- main.go
+++ main.go
@@ -323,7 +323,7 @@ var rootCmd = &cobra.Command{
 	Use:     "stringx [flags] [FILE...]",
 	Short:   "Finds printable strings in a file, with advanced filtering.",
 	Long:    `An enhanced version of the classic 'strings' utility, written in Go.`,
-	Version: "2.0.0",
+	Version: "0.1.1",
 
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 
