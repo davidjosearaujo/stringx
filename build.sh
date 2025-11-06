2ac9b13e20cdede1805f97ddb23a145388b58d3a
David Araújo <github@davidjosearaujo.dev>
2025-11-06 21:38:09 +0000

    Add checksum generation to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
+rm -rf build
+
 go build -o build/stringx-linux-x86-amd64 -ldflags="-s -w" .
 GOOS=darwin GOARCH=arm64 go build -o build/stringx-darwin-arm64 -ldflags="-s -w" .
 GOOS=windows GOARCH=amd64 go build -o build/stringx-windows-x86.exe -ldflags="-s -w" .
 GOOS=linux GOARCH=arm64 go build -o build/stringx-linux-arm64 -ldflags="-s -w" .
 GOOS=linux GOARCH=386 go build -o build/stringx-linux-386 -ldflags="-s -w" .
 GOOS=freebsd GOARCH=amd64 go build -o build/stringx-freebsd-x86-amd64 -ldflags="-s -w" .
+
+for file in $(ls build | cat); do
+    sha256sum build/$file | awk '{split($2,a,"/"); print $1, a[length(a)]}' >> build/checksums.txt
+done
